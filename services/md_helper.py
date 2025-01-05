6eeb6418ad337eedb5b8129a511c4d0195e7af34
samdx <18287802+samdx@users.noreply.github.com>
2025-01-05 17:56:44 +0800

    md_helper_lab

--- services/md_helper.py
+++ services/md_helper.py
@@ -48,3 +48,35 @@ class MDHelper:
                 markdown.append(f"## Step {step_number}: {step_text}")
 
         return "\n\n".join(markdown) + "\n"
+
+    @staticmethod
+    def md_helper_lab(data: Dict) -> str:
+        markdown = []
+
+        # Add front matter
+        front_matter_lines = ["---"]
+        for key, value in data.items():
+            if key == "description" or key == 'steps':
+                continue
+
+            if key == "name":
+                front_matter_lines.append(f"{key}: '{value}'")
+                continue
+
+            front_matter_lines.append(f"{key}: {value}")
+        front_matter_lines.append("---")
+        markdown.append("\n".join(front_matter_lines))
+
+        # The # main heading
+        markdown.append(f"# [{data['name']}]({data['url']})")
+
+        # Add description
+        if "description" in data:
+            markdown.append(f"{data['description']}")
+        
+        # Add steps belong to a Lab, each step is a sub-heading at level 2
+        if data['type'] == 'Lab' and 'steps' in data:
+            for step_number, step_text in data['steps'].items():
+                markdown.append(f"## Step {step_number}: {step_text}")
+
+        return "\n\n".join(markdown) + "\n"
