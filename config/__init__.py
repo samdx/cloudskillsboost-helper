1475f0e3baaeb8bbb25a50c6cd342b8c1eb13f75
samdx <18287802+samdx@users.noreply.github.com>
2025-01-05 17:22:48 +0800

    Update __init__.py

---
+++ config/__init__.py
@@ -0,0 +1,12 @@
+# Description: Configuration file for the project
+
+# Output folder name
+OUTPUT_FOLDER_NAME: str = '../cloudskillsboost_md'
+DATA_FOLDER_NAME: str = "data"
+
+# Base URL for the Cloud Skills Boost website
+BASE_URL: str = "https://www.cloudskillsboost.google"
+BASE_URL_PATHS: str = f"{BASE_URL}/paths"
+BASE_URL_LAB: str = f"{BASE_URL}/catalog_lab"
+BASE_URL_COURSES: str = f"{BASE_URL}/course_templates"
+BASE_URL_PARTNERS: str = "https://partner.cloudskillsboost.google"
