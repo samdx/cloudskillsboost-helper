eafcb07268c3a900e8d5d08472bb6dc9acf574e4
samdx <18287802+samdx@users.noreply.github.com>
2025-01-05 18:51:41 +0800

    md_helper_course

--- services/md_helper.py
+++ services/md_helper.py
@@ -1,5 +1,6 @@
 from typing import Dict
-from utils.utils import util_replace_special_chars  # Correct import
+from config.settings import BASE_URL
+from utils.utils import util_replace_quote_marks, util_replace_special_chars  # Correct import
 
 class MDHelper:
 
@@ -79,4 +80,103 @@ class MDHelper:
             for step_number, step_text in data['steps'].items():
                 markdown.append(f"## Step {step_number}: {step_text}")
 
+        return "\n\n".join(markdown) + "\n"
+
+    @staticmethod
+    def md_helper_course(data: Dict) -> str:
+        markdown = []
+
+        # Add front matter
+        front_matter_lines = ["---"]
+        for key, value in data.items():
+            if key == "description" or key == "objectives" or key == "modules":
+                continue
+
+            if key == "topics" and data['topics']:
+                front_matter_lines.append(f"{key}:")
+                for topic in value:
+                    front_matter_lines.append(f"- {topic}")
+                continue
+
+            if key == "name":
+                front_matter_lines.append(f"{key}: '{value}'")
+                continue
+
+            front_matter_lines.append(f"{key}: {value}")
+        front_matter_lines.append("---")
+        markdown.append("\n".join(front_matter_lines))
+        # End of front matter
+
+        # The # Heading
+        markdown.append(f"# [{data['name']}]({data['url']})")
+
+        # Add description
+        if "description" in data and data.get('description') is not None:
+            markdown.append("**Description:**")
+            markdown.append(f"{data['description']}")
+
+        # Add objectives, \n between each item
+        if "objectives" in data and len(data.get("objectives")) > 0:
+            markdown.append("**Objectives:**")
+            objective_list = []
+            for objective in data['objectives']:
+                objective_list.append(f"- {objective}")
+            markdown.append("\n".join(objective_list))
+            # markdown.append(data['objectives'])
+        # End of objectives
+
+        # If modules in a course
+        if "modules" in data:
+            modules = data['modules']
+            for module in modules:
+                # Each module is a level 2 heading
+                markdown.append(f"## {module['title']}")
+
+                if module['description']:
+                    markdown.append(f"{module['description'].strip('<p>').strip('</p>')}")
+
+                for step in module['steps']:
+                    for activity in step['activities']:
+                        activity_title = activity['title']
+                        activity_type = activity['type']
+                        activity_href = activity['href']
+                        # Each activity is a level 3 heading
+                        markdown.append(f"### {activity_type.title()} - [{activity_title}]({BASE_URL}{activity_href})")
+
+                        # If it's a video, let put a YouTube video for it here.
+                        if activity_type == 'video':
+                            markdown.append(f"- [YouTube: {activity_title}](https://www.youtube.com/watch?v={activity['videoId']})")
+                            markdown.append(activity['transcript'])
+
+                        # If it's a video, let put a checkbox here for a completion status
+                        elif activity_type == 'lab':
+                            markdown.append(activity.get('description'))
+                            lab_md_name = f"{util_replace_special_chars(activity_title)}.md"
+                            if activity['isComplete'] is False:
+                                markdown.append(f"- [ ] [{activity_title}](../labs/{lab_md_name})")
+                            else:
+                                markdown.append(f"- [x] [{activity_title}](../labs/{lab_md_name})")
+
+                        # If it's a quiz, presents all the quiz and answer here
+                        elif activity_type == 'quiz':
+                            quizItems = activity['quizItems']
+                            quiz_number = 1
+                            for question in quizItems:
+                                quiz_list = []
+                                quiz_stem = question.get("stem").replace("<p>", "").replace("</p>", "")
+                                quiz_stem = quiz_stem.replace('\n\n', '')
+                                # Each quiz is a level 4 heading
+                                markdown.append(f"#### Quiz {quiz_number}.")
+                                # Put into a callout format for Obsidian or Typora
+                                quiz_list.append(f"> [!important]")
+                                quiz_list.append(f"> {util_replace_quote_marks(quiz_stem)}**")
+                                quiz_list.append(">")
+                                for option in question.get("options"):
+                                    quiz_list.append(f"> - [ ] {util_replace_quote_marks(option.get('title'))}")
+                                markdown.append("\n".join(quiz_list))
+                                quiz_number += 1
+
+        # End of modules
+
+        # Ensure an empty line between each section, and an extra empty line at the end of the file
         return "\n\n".join(markdown) + "\n"
