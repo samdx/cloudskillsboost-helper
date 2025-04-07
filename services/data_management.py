86d48434c04bf38d85cec944f03296eac8f5e395
samdx <18287802+samdx@users.noreply.github.com>
2025-04-07 11:11:24 +0800

    Create data_management.py

---
+++ services/data_management.py
@@ -0,0 +1,25 @@
+
+class DataManagement():
+    
+    def fetch_path_data(path_id):
+        
+        """
+        Fetch path data based on the provided path_id.
+        """
+
+        pass
+
+    def fetch_course_data(course_id):
+
+        """
+        Fetch course data from the website and save it to a JSON file.
+        """
+
+        pass
+
+    def fetch_lab_data(lab_id):
+        """
+        Fetch lab data based on the provided lab_id.
+        """
+
+        pass
