0a70f11d6d500720a158232b3b9b20307b03e2bc
xuansam <xuansam@google.com>
2025-04-14 02:27:49 +0000

    Added extensions into .nix config.

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -7,7 +7,7 @@
   # Use https://search.nixos.org/packages to find packages
   packages = [
     # pkgs.go
-    # pkgs.python311
+    pkgs.python311
     # pkgs.python311Packages.pip
     # pkgs.nodejs_20
     # pkgs.nodePackages.nodemon
@@ -19,6 +19,8 @@
     # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
     extensions = [
       # "vscodevim.vim"
+      "ms-python.debugpy"
+      "ms-python.python"
     ];
 
     # Enable previews
